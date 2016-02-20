ae1c54b12d22d4c2907de9c4c8bdf53dc295751a
ThomasKearney <thomasnkearney@gmail.com>
2016-02-20 01:00:17 -0600

    wrong folder..

--- matrix_test.py
+++ matrix_test.py
@@ -11,24 +11,17 @@ from rgbmatrix import Adafruit_RGBmatrix
 # Rows and chain length are both required parameters:
 matrix = Adafruit_RGBmatrix(32, 1)
 
-# Flash screen red, green, blue (packed color values)
-matrix.Fill(0xFF0000)
-time.sleep(1.0)
-matrix.Fill(0x00FF00)
-time.sleep(1.0)
-matrix.Fill(0x0000FF)
-time.sleep(1.0)
+
 
 # Show RGB test pattern (separate R, G, B color values)
-for b in range(16):
-  for g in range(8):
-    for r in range(8):
-	    matrix.SetPixel(
-	      (b / 4) * 8 + g,
-	      (b & 3) * 8 + r,
-	      (r * 0b001001001) / 2,
-	      (g * 0b001001001) / 2,
-	       b * 0b00010001)
+for i in range(0,31):
+	SetPixel(i, 16, 255, 255, 255)
+	if i > 0: 
+		SetPixel(i-1, 16, 200, 200, 200)
+	if i > 1
+		SetPixel(i-2, 16, 100, 100, 100)
+	time.sleep(0.1)
+	matrix.clear()
 
 time.sleep(10.0)
 matrix.Clear()
