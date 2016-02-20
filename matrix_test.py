d64b7088e2cc69b02fb76ed77e8711b4df9a89ff
Nick M <nmont16@gmail.com>
2016-02-20 08:20:13 +0000

    display image

--- matrix_test.py
+++ matrix_test.py
@@ -10,18 +10,10 @@ from rgbmatrix import Adafruit_RGBmatrix
 
 # Rows and chain length are both required parameters:
 matrix = Adafruit_RGBmatrix(32, 1)
-image = Image.new("1", (32, 32)) 
-draw  = ImageDraw.Draw(image)    
+image = Image.open("nick.jpg")
+image.load()
+matrix.SetImage(image.im.id, 0, 0)
 
-
-# Show RGB test pattern (separate R, G, B color values)
-draw.text((0, 16), "Fuck you bowler", fill=1)
-
-for n in range(-128, 128): # Start off top-left, move off bottom-right
-	matrix.Clear()
-	# IMPORTANT: *MUST* pass image ID, *NOT* image object!
-	matrix.SetImage(image.im.id, -n, 0)
-	time.sleep(0.05)
-
+time.sleep(10)
 
 matrix.Clear()
