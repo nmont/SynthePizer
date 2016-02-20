2b39abb331db8ebf4436eb1cf9d38ad0720c7a4b
Nick Montelibano <nmont@iastate.edu>
2016-02-20 08:32:44 -0600

    Adding test graphic

---
+++ graphicTests/rotating_dot.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+# Simple RGBMatrix example, using only Clear(), Fill() and SetPixel().
+# These functions have an immediate effect on the display; no special
+# refresh operation needed.
+# Requires rgbmatrix.so present in the same directory.
+
+import time
+from rgbmatrix import Adafruit_RGBmatrix
+
+# Rows and chain length are both required parameters:
+matrix = Adafruit_RGBmatrix(32, 1)
+
+
+
+def draw_touch(x, y, radius):
+        
+        set_point(x-1, y-1)
+        set_point(x-1, y+1)
+        set_point(x+1, y-1)
+        set_point(x+1, y+1)
+        time.sleep(3)
+	
+def set_point(x, y):
+
+        matrix.SetPixel(
+          x,
+          y,
+          (2 * 0b001001001) / 2,
+          (2 * 0b001001001) / 2,
+          2 * 0b00010001)
+
+
+for x in range(12)
+
+        draw_touch(x, 10, 10)
+        time.sleep(2.0)
+        matrix.Clear()
+
