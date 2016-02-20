052d0fdb8861f18b193c3d69c2b757ffa21e2409
Andrew Bowler <ajbowler@iastate.edu>
2016-02-19 22:54:35 -0600

    Light up entire matrix column and one pixel next to y value

--- matrix_main.py
+++ matrix_main.py
@@ -4,8 +4,37 @@ import time
 from rgbmatrix import Adafruit_RGBmatrix
 
 matrix = Adafruit_RGBmatrix(32, 1)
+xCoord = ''
+yCoord = ''
+pressureValue = ''
+shouldLightUpRight = false
+shouldLightUpLeft = false
 
-matrix.Fill(0xFFFFFF)
+
+def parseX(x):
+	if (x > 41.6):
+		xCoord = 32
+		shouldLightUpRight = True
+	elif (x < 9.6):
+		xCoord = -32
+		shouldLightUpLeft = True
+	else:
+		xCoord = int(x)
+
+def parseTouch(x, y, pressure):
+	parseX(x)
+	yCoord = int(y)
+
+def lightUpRight(x, y):
+	for yPixel in range(32):
+		matrix.SetPixel(x, yPixel, 1, 0, 0)
+	matrix.SetPixel(x - 1, y, 1, 0, 0)
+
+parseTouch(50, 12)
+
+if (shouldLightUpRight):
+	lightUpRight(xCoord, yCoord)
+
 time.sleep(3.0)
 
 matrix.Clear()
