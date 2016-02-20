919ec284546b4a1b7b2d0dc03f922e60f0c9dbbb
Nick M <nmont16@gmail.com>
2016-02-20 18:01:50 +0000

    WORKING input to matrix

--- boogieBoardDriver/usb-driver.py
+++ boogieBoardDriver/usb-driver.py
@@ -4,7 +4,8 @@ import usb.core
 import usb.util
 import sys
 from evdev import UInput, AbsInfo, ecodes as e
-import time
+from time import sleep
+from numpy import floor
 from rgbmatrix import Adafruit_RGBmatrix
 
 def draw_touch(counter, x, y):
@@ -16,14 +17,16 @@ def draw_touch(counter, x, y):
 
   tup1 = (x-1, x, x+1, x+1, x+1, x, x-1, x-1)
   tup2 = (y-1, y-1, y-1, y, y+1, y+1, y+1, y)
-  set_point(tup1[counter % 8], tup2[counter % 8], r1, b2)
-  set_point(tup1[(counter + 1)], tup2[(counter + 1)], r1, b2)
-  set_point(tup1[(counter + 2)], tup2[(counter + 2)], r1, b2)
-  set_point(tup1[(counter + 3)], tup2[(counter + 3)], r1, b2)
-  set_point(tup1[(counter + 4)], tup2[(counter + 4)], r2, b1)
-  set_point(tup1[(counter + 5)], tup2[(counter + 5)], r2, b1)
-  set_point(tup1[(counter + 6)], tup2[(counter + 6)], r2, b1)
-  set_point(tup1[(counter + 7)], tup2[(counter + 7)], r2, b1)
+  set_point(x, y, r1, b2)
+  #set_point(tup1[counter % 8], tup2[counter % 8], r1, b2)
+  #set_point(tup1[(counter + 1)], tup2[(counter + 1)], r1, b2)
+  #set_point(tup1[(counter + 2)], tup2[(counter + 2)], r1, b2)
+  #set_point(tup1[(counter + 3)], tup2[(counter + 3)], r1, b2)
+  #set_point(tup1[(counter + 4)], tup2[(counter + 4)], r2, b1)
+  #set_point(tup1[(counter + 5)], tup2[(counter + 5)], r2, b1)
+  #set_point(tup1[(counter + 6)], tup2[(counter + 6)], r2, b1)
+  #set_point(tup1[(counter + 7)], tup2[(counter + 7)], r2, b1)
+  #print("Here\n")
 
 
 def set_point(x, y, r, b):
@@ -33,6 +36,7 @@ def set_point(x, y, r, b):
     r,
     (2 * 0b001001001) / 2,
     b)
+  sleep(0.05)
 
 
 matrix = Adafruit_RGBmatrix(32, 1)
@@ -76,18 +80,6 @@ maxypos = 13442
 minpressure = 0
 maxpressure = 255
 
-# Initialise UInput device
-cap = {
-    e.EV_KEY : (e.BTN_TOUCH, e.BTN_STYLUS2),
-    e.EV_ABS : [
-        # N.B.: There appears to be a mapping bug here; setting min to max results in
-        # setting max when reading back ui capabilities.
-        (e.ABS_PRESSURE, AbsInfo(value=minpressure, min=maxpressure, max=0, fuzz=0, flat=0, resolution=0)),
-        (e.ABS_X, AbsInfo(value=minxpos, min=maxxpos, max=0, fuzz=0, flat=0, resolution=0)),
-        (e.ABS_Y, AbsInfo(value=minypos, min=maxypos, max=0, fuzz=0, flat=0, resolution=0))]
-}
-# ui = UInput(cap, name='boogie-board-sync-pen')
-
 counter = 0
 
 try:
@@ -119,19 +111,13 @@ try:
         touch = data[7] & 0x01
         stylus = (data[7] & 0x02)>>1
         
-        xpos = math.floor(xpos / (maxypos / 32))
-        ypos = math.floor(ypos / (maxypos / 32))
+        xpos = int(floor(xpos / (maxypos / 32)))
+        ypos = int(floor(ypos / (maxypos / 32)))
         
-        # ui.write(e.EV_ABS, e.ABS_PRESSURE, pressure)
-        # ui.write(e.EV_ABS, e.ABS_X, xpos)
-        # ui.write(e.EV_ABS, e.ABS_Y, ypos)
-        # ui.write(e.EV_KEY,e.BTN_TOUCH,touch)
-        # ui.write(e.EV_KEY,e.BTN_STYLUS2,stylus)
-        # ui.syn()
         #print('xpos: %5d ypos: %5d pressure: %3d' % (xpos, ypos, pressure))
         draw_touch(counter, xpos, ypos)
         counter = (counter + 1) % 8
-        time.sleep(.5)
+        # sleep(5)
         matrix.Clear()
         # print('touch: %d stylus %d' % (touch, stylus))
 except KeyboardInterrupt:
