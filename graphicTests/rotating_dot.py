072222f11917e76d57f80aa2e12d2d040e14f0ef
Nick Montelibano <nmont@iastate.edu>
2016-02-20 08:38:23 -0600

    Rotating Point Test Fix2

--- graphicTests/rotating_dot.py
+++ graphicTests/rotating_dot.py
@@ -21,7 +21,7 @@ def draw_touch(x, y, radius):
                 set_point(x+1, y-1)
                 set_point(x+1, y+1)
         
-                time.sleep(.1)
+                time.sleep(.2)
                 matrix.Clear()
                 
                 set_point(x-1, y)
@@ -29,7 +29,7 @@ def draw_touch(x, y, radius):
                 set_point(x+1, y)
                 set_point(x, y-1)
                 
-                time.sleep(.1)
+                time.sleep(.2)
                 matrix.Clear()
 	
 def set_point(x, y):
@@ -42,9 +42,8 @@ def set_point(x, y):
           2 * 0b00010001)
 
 
-for x in range(12):
+for x in range(1:12):
 
         draw_touch(x, 10, 10)
-        time.sleep(2.0)
         matrix.Clear()
 
