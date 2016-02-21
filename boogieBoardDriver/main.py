a943bd693f2e930c42727aa1cecb278e7e6b7122
Andrew Bowler <ajbowler@iastate.edu>
2016-02-20 21:33:59 -0600

    Refactor and clean up main file, rename to main.py

--- boogieBoardDriver/main.py
+++ boogieBoardDriver/main.py
@@ -5,7 +5,6 @@ import usb.util
 import sys
 import Image
 import ImageDraw
-#from test_audio import Audio
 from evdev import UInput, AbsInfo, ecodes as e
 from time import sleep
 from numpy import floor
@@ -15,12 +14,12 @@ import sys
 
 # recognizing SIGING (ctrl + c)
 def signal_handler(signal, frame):
-        usb.util.release_interface(dev, 0)
-        usb.util.release_interface(dev, 1)
-        dev.attach_kernel_driver(0)
-        dev.attach_kernel_driver(1)
-	print('Cancelling Awesomeness')
-        sys.exit(0)
+  usb.util.release_interface(dev, 0)
+  usb.util.release_interface(dev, 1)
+  dev.attach_kernel_driver(0)
+  dev.attach_kernel_driver(1)
+  print('Cancelling Awesomeness')
+  sys.exit(0)
 
 def draw_touch(counter, x, y, stylusButtonDown):
   r1 = 0b11111111
@@ -30,23 +29,21 @@ def draw_touch(counter, x, y, stylusButtonDown):
   b2 = 0
 
   # tuples of xy inputs for ring cursor
-  tup1 = (x-1, x, x+1, x+1, x+1, x, x-1, x-1)
-  tup2 = (y-1, y-1, y-1, y, y+1, y+1, y+1, y)
+  cursor_arr_x = (x-1, x, x+1, x+1, x+1, x, x-1, x-1)
+  cursor_arr_y = (y-1, y-1, y-1, y, y+1, y+1, y+1, y)
 
   # light up the middle if the stylus button is down
   if stylusButtonDown:
     matrix.SetPixel(x, y, 255, 255, 255)
 
   # cursor ring
-  matrix.SetPixel(tup1[(counter + 1) % 8], tup2[(counter + 1) % 8], r1, 0, b2)
-  matrix.SetPixel(tup1[(counter + 2) % 8], tup2[(counter + 2) % 8], r1, 0, b2)
-  matrix.SetPixel(tup1[(counter + 3) % 8], tup2[(counter + 3) % 8], r1, 0, b2)
-  matrix.SetPixel(tup1[(counter + 4) % 8], tup2[(counter + 4) % 8], r2, 0, b1)
-  matrix.SetPixel(tup1[(counter + 5) % 8], tup2[(counter + 5) % 8], r2, 0, b1)
-  matrix.SetPixel(tup1[(counter + 6) % 8], tup2[(counter + 6) % 8], r2, 0, b1)
-  matrix.SetPixel(tup1[(counter + 7) % 8], tup2[(counter + 7) % 8], r2, 0, b1)
- # a = Audio("didgi-7.wav")
- # a.play(True)
+  matrix.SetPixel(cursor_arr_x[(counter + 1) % 8], cursor_arr_y[(counter + 1) % 8], r1, 0, b2)
+  matrix.SetPixel(cursor_arr_x[(counter + 2) % 8], cursor_arr_y[(counter + 2) % 8], r1, 0, b2)
+  matrix.SetPixel(cursor_arr_x[(counter + 3) % 8], cursor_arr_y[(counter + 3) % 8], r1, 0, b2)
+  matrix.SetPixel(cursor_arr_x[(counter + 4) % 8], cursor_arr_y[(counter + 4) % 8], r2, 0, b1)
+  matrix.SetPixel(cursor_arr_x[(counter + 5) % 8], cursor_arr_y[(counter + 5) % 8], r2, 0, b1)
+  matrix.SetPixel(cursor_arr_x[(counter + 6) % 8], cursor_arr_y[(counter + 6) % 8], r2, 0, b1)
+  matrix.SetPixel(cursor_arr_x[(counter + 7) % 8], cursor_arr_y[(counter + 7) % 8], r2, 0, b1)
   sleep(0.05)
 
 # ============== MAIN ==========================
@@ -57,33 +54,30 @@ signal.signal(signal.SIGINT, signal_handler)
 # find our device
 dev = usb.core.find(idVendor=0x2914, idProduct=0x0100)
 if dev is None:
-    print("Dev is Null. Try Again")
-    sys.exit(1)
+  print("Dev is Null. Try Again")
+  sys.exit(1)
 if dev.is_kernel_driver_active(1):
-    print("passed dev.detach")
-    dev.detach_kernel_driver(1)
+  print("passed dev.detach")
+  dev.detach_kernel_driver(1)
 if dev.is_kernel_driver_active(0):
-    try:
-        dev.detach_kernel_driver(0)
-    except:
-        dev.attach_kernel_driver(1)
+  try:
+    dev.detach_kernel_driver(0)
+  except:
+    dev.attach_kernel_driver(1)
 
 print('about to send payloud')
-#dev.set_configuration()
-#usb.util.claim_interface(dev, 1)
-#usb.util.claim_interface(dev, 0)
 
 # knock into tablet mode
 payload = '\x05\x00\x03'
 while True:
-    try:
-        assert dev.ctrl_transfer(0x21, 0x09, 0x0305, 1, payload, 100) == len(payload)
-        print('sent payload')
-        break
-    except usb.USBError as err:
-        if err.args != (110, 'Operation timed out') and err.args != (32, 'Pipe error'):
-            raise err
-        print('payload transfer failed, retrying')
+  try:
+    assert dev.ctrl_transfer(0x21, 0x09, 0x0305, 1, payload, 100) == len(payload)
+    print('sent payload')
+    break
+  except usb.USBError as err:
+    if err.args != (110, 'Operation timed out') and err.args != (32, 'Pipe error'):
+      raise err
+    print('payload transfer failed, retrying')
 print('Payload sent')
 
 # Pull out interrupt endpoint
@@ -110,8 +104,6 @@ PLAY_TROMBONE = "PLAY_TROMBONE"
 state = MAIN_MENU
 
 main_image = Image.open("../assets/mainmenu.jpg")
-dick_butt_right_image = Image.open("../assets/dickbuttright.jpg")
-dick_butt_left_image = Image.open("../assets/dickbuttleft.jpg")
 didgeridoo_image = Image.open("../assets/didgeridoo.jpg")
 trombone_image = Image.open("../assets/trombone.jpg")
 
@@ -123,27 +115,27 @@ is_touched = False
 while True:
     try:
       #bring in data from the boogie board
-        data = ep.read(8, 100)
+      data = ep.read(8, 100)
     except usb.USBError as err:
-        if err.args != (110, 'Operation timed out'):
-            raise err
-        continue
+      if err.args != (110, 'Operation timed out'):
+        raise err
+      continue
 
     xpos = data[1] | data[2] << 8
     ypos = data[3] | data[4] << 8
 
     if xpos < minxpos:
-        minxpos = xpos
-        print('updated minxpos to %d' % minxpos)
+      minxpos = xpos
+      print('updated minxpos to %d' % minxpos)
     if xpos > maxxpos:
-        maxxpos = xpos
-        print('updated maxxpos to %d' % maxxpos)
+      maxxpos = xpos
+      print('updated maxxpos to %d' % maxxpos)
     if ypos < minypos:
-        minypos = ypos
-        print('updated minypos to %d' % minypos)
+      minypos = ypos
+      print('updated minypos to %d' % minypos)
     if ypos > maxypos:
-        maxypos = ypos
-        print('updated maxypos to %d' % maxypos)
+      maxypos = ypos
+      print('updated maxypos to %d' % maxypos)
 
     pressure = data[5] | data[6] << 8
     touch = data[7] & 0x01
@@ -181,16 +173,14 @@ while True:
       if touch:
         draw_touch(counter, xpos, ypos, stylus)
         counter = (counter + 1) % 8
+        
         # scroll through selections
+        # return to main menu
         if xpos == 0 and ypos == 0 and stylus:
           is_touched = True
           state = MAIN_MENU
-        elif xpos > 16 and ypos < 25 and stylus and not is_touched:
-          is_touched = True
-          image_count = (image_count + 1) % num_images
-        elif xpos <= 16 and ypos < 25 and stylus and not is_touched:
-          is_touched = True
-          image_count = (image_count - 1) % num_images
+
+        # select instrument
         elif ypos >= 25 and stylus and not is_touched:
           is_touched = True
           if image_count == 0:
@@ -200,6 +190,18 @@ while True:
           else:
             print('Image count error')
             state = MAIN_MENU
+
+        # scroll right
+        elif xpos > 16 and stylus and not is_touched:
+          is_touched = True
+          image_count = (image_count + 1) % num_images
+        
+        # scroll left
+        elif xpos <= 16 and stylus and not is_touched:
+          is_touched = True
+          image_count = (image_count - 1) % num_images
+
+        # not touching the board
         elif not stylus and is_touched:
           is_touched = False
 
